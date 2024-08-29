9089ef4e9e449bda6f732375fa3dc5cd3b277f2e
giovanirogerio02 <Geovannirogeriogr@gmail.com>
2024-08-29 17:06:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2> Cadastro de usuário </h2>
+
+    <form action="processa_cadastro.php" method="post">
+        
+        <label for="nome"> Nome </label><br>
+        <input type="text" id="nome" name="nome" required> <br> <br>
+        
+        <label for="Email"> Email </label></br>
+        <input type="email" id="email" name="email" required> <br> <br>
+
+        <label for="Senha"> Senha </label></br>
+        <input type="password" id="senha" name="senha" required> <br> <br>
+
+        <input type="submit" value="Cadastrar">
+
+    </form>
+
+
+</body>
+</html>
